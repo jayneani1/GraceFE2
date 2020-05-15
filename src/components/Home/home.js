5c68e91379c6ca5d303d09bae13514fa3d48f831
jayneani <jayneaniello27@gmail.com>
2020-05-15 01:18:48 -0500

    fix

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -15,7 +15,7 @@ const Home = () => {
     marginTop: '50px',
     color: "#E1D8CE",
     alignItems: "center"}}>
-       <a href="/main"><img style={{margin: "2% auto",
+       <a href="/main"><img className="enterImage" style={{margin: "1% auto",
       alignItems: "center",
       justifyContent: "center"}} src="https://res.cloudinary.com/dnxx8igwb/image/upload/v1589479717/imageedit_3_8065322209_i3edqx.png" alt="enter"></img></a>
       <Route path="/main" cmponent={Main}/>
